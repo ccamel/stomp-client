290636f583fb1b8d04ca80b5f0bc54cb10e365c7
Chris Camel <camel.christophe@gmail.com>
2017-04-12 12:50:27 +0200

    add missing BEGIN STOMP frame

---
+++ src/main/scala/me/ccm/stomp/protocol/frames/client/begin.scala
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Chris Camel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package me.ccm.stomp.protocol.frames.client
+
+import me.ccm.stomp.protocol.frames.{CommonHeaderKeys, StompFrame}
+
+trait BeginHeaderKeys extends ClientHeaderKeys with CommonHeaderKeys {
+  /** The Constant TRANSACTION. */
+  val TRANSACTION: String = "transaction"
+}
+
+case class Begin(transaction: String,
+               additionalHeaders: Map[String, String] = Map.empty) extends StompFrame {
+
+  override val frameName: String = Begin.frameName
+
+  override val headers: Map[String, String] =
+    additionalHeaders ++
+      Some(Begin.TRANSACTION -> transaction)
+
+  override def body: Array[Byte] = Array.emptyByteArray
+}
+
+object Begin extends ClientFrameProps with BeginHeaderKeys {
+  val frameName: String = "BEGIN"
+
+  override def hasBody: Boolean = false
+
+  def apply(headers: Map[String, String]): Begin = {
+    val transaction = headers(TRANSACTION)
+    val additionalHeaders = headers - (TRANSACTION)
+
+    Begin(transaction, additionalHeaders)
+  }
+}
---
+++ src/test/scala/me/ccm/stomp/protocol/frames/client/BeginTest.scala
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Chris Camel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.ccm.stomp.protocol.frames.client
+
+import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+import scala.Array.emptyByteArray
+
+/**
+  *
+  */
+class BeginTest extends FlatSpec with Matchers with GivenWhenThen with TableDrivenPropertyChecks {
+  behavior of "The BEGIN frame"
+
+  val transaction = "tx1"
+
+  it must "be named begin" in {
+    val f = Begin(transaction)
+
+    Begin.frameName should be("BEGIN")
+    f.frameName should be("BEGIN")
+  }
+
+  it must "include a transaction header" in {
+    val f = Begin(transaction)
+
+    Begin.TRANSACTION should be("transaction")
+    f.transaction should equal(transaction)
+    f.headers should contain(Begin.TRANSACTION -> transaction)
+    f.additionalHeaders should not contain key(Begin.TRANSACTION)
+  }
+
+  it must "not have a body" in {
+    val f = Begin("123")
+
+    Begin.hasBody should be(false)
+    f.body should be(emptyByteArray)
+    f.hasBodyContent should be(false)
+  }
+
+  it must "be correctly constructed from a map" in {
+    val data =
+      Table(
+        ("map", "result"),
+        (Map("transaction" -> transaction), Begin(transaction)),
+        (Map("transaction" -> transaction, "a" -> "1"), Begin(transaction, Map("a" -> "1")))
+      )
+
+    forAll(data) { (m: Map[String, String], result: Begin) =>
+      Given(s"the map $m")
+      Then(s"Begin is $result")
+      Begin(m) should equal(result)
+    }
+
+    a[NoSuchElementException] should be thrownBy {
+      Begin(Map("foo" -> "bar"))
+    }
+  }
+}
